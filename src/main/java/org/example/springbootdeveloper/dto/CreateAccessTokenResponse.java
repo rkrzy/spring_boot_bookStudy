c615538f092df1d7533d669abf414cdbb66ee514
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-02-13 14:32:08 +0900

    9장 : JWT로 로그인/로그아웃 구현하기

---
+++ src/main/java/org/example/springbootdeveloper/dto/CreateAccessTokenResponse.java
@@ -0,0 +1,11 @@
+package org.example.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CreateAccessTokenResponse {
+    private String accessToken;
+
+}
